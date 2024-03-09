2458739cc6691c6989b25ecab59858cbf91f3d01
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-03-09 11:04:16 +0000

    Finished first part

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
-const randomNumber = Math.round(Math.random() * 10)
+//Variables
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
 
-let xAttempts = 0
+let randomNumber = Math.round(Math.random() * 10)
 
-function handleClick (event) {
+let xAttempts = 1
+
+//Events
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')){
+    handleResetClick()
+  }
+})
+
+//Callback Functions
+function handleTryClick (event) {
     event.preventDefault()
-
     const inputNumber = document.querySelector('#inputNumber')
-
     if(Number(inputNumber.value)== randomNumber) {
-      document.querySelector(".screen1").classList.add('hide')
-      document.querySelector(".screen2").classList.remove('hide')
-
+      toggleScreen()
       document
         .querySelector(".screen2 h2")
         .innerText = `Acertou em ${xAttempts} tentativas`
     }
-
+    inputNumber.value = ""
     xAttempts++
-
-    console.log(xAttempts)
 }
+
+function handleResetClick () {
+  toggleScreen()
+  xAttempts=1
+  randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
+
+
